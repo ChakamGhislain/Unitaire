fdb178565c4e54d0bd5eaf8c96ffdc992b3ff0e4
ChakamGhislain <ghislain.chakam@gmail.com>
2022-01-31 22:14:33 +0100

    New commit after changed my file workflows

--- tests/MathTest.php
+++ tests/MathTest.php
@@ -5,7 +5,7 @@ class MathTest extends PHPUnit\Framework\TestCase{
 
     public function testDouble(){
 
-        $this->assertEquals(4, \Chapitou\Math::double(2), "Le resultat devrait être 4" );
+        $this->assertEquals(7, \Chapitou\Math::double(2), "Le resultat devrait être 4" );
     }
-    //Jai changé la valeur 6 en 4 Test reussi
+    
 }
