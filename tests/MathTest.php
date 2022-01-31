1b1302e5bc1622f4ef14aafd3f5e441cbf554ca9
ChakamGhislain <ghislain.chakam@gmail.com>
2022-01-31 22:17:30 +0100

    Test validé apres modification de la valeur de retour

--- tests/MathTest.php
+++ tests/MathTest.php
@@ -5,7 +5,7 @@ class MathTest extends PHPUnit\Framework\TestCase{
 
     public function testDouble(){
 
-        $this->assertEquals(7, \Chapitou\Math::double(2), "Le resultat devrait être 4" );
+        $this->assertEquals(4, \Chapitou\Math::double(2), "Le resultat devrait être 4" );
     }
     
 }
