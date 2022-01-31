620fdd1a76fe891ebb31e2698487eb9bcb794674
ChakamGhislain <ghislain.chakam@gmail.com>
2022-01-31 13:19:26 +0100

    First commit PhpUnit

---
+++ tests/MathTest.php
@@ -0,0 +1,10 @@
+<?php
+
+
+class MathTest extends PHPUnit\Framework\TestCase{
+
+    public function testDouble(){
+
+        $this->assertEquals(4, \Chapitou\Math::double(2), "Le resultat devrait être 4" );
+    }
+}
